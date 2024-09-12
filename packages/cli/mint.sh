320290cc8b3fad0150f6c239a2e60fc61c645a70
1024btc <saltwore@gmail.com>
2024-09-12 16:19:22 +0800

    feat: 添加交互

--- packages/cli/mint.sh
+++ packages/cli/mint.sh
@@ -3,25 +3,30 @@
 # https://mempool.fractalbitcoin.io/api/v1/fees/recommended
 # bash 请求 fee
 
-while true; do
-    response=$(curl -s https://mempool.fractalbitcoin.io/api/v1/fees/recommended)
-    
-    # 经济的gas文案
-    # response=$(curl -s https://mempool.fractalbitcoin.io/api/v1/fees/mempool-blocks)
-    # feeRate=$(echo $response | jq '.[0].feeRange | .[2]') # 倒数第三档
+# 获取用户设置的mint次数
+read -p "请输入 mint 次数: " MINT_COUNT
 
-    feeRate=$(echo $response | jq '.fastestFee')
-    echo $feeRate
+response=$(curl -s https://mempool.fractalbitcoin.io/api/v1/fees/recommended)
 
-    # 如果没有获取到 feeRate，使用config里面max的配置
-    if [ -z "$feeRate" ] || [ "$feeRate" == "null" ]; then
-        feeRate=''
-    else
-        feeRate='--fee-rate '$feeRate
-    fi
+# 经济的gas文案
+# response=$(curl -s https://mempool.fractalbitcoin.io/api/v1/fees/mempool-blocks)
+# feeRate=$(echo $response | jq '.[0].feeRange | .[2]') # 倒数第三档
 
-    echo $feeRate
-    command="yarn cli mint -i 45ee725c2c5993b3e4d308842d87e973bf1951f5f7a804b21e4dd964ecd12d6b_0 5 $feeRate"
+feeRate=$(echo $response | jq '.fastestFee')
+echo $feeRate
+
+# 如果没有获取到 feeRate，使用config里面max的配置
+if [ -z "$feeRate" ] || [ "$feeRate" == "null" ]; then
+    feeRate=''
+else
+    feeRate='--fee-rate '$feeRate
+fi
+
+echo $feeRate
+command="yarn cli mint -i 45ee725c2c5993b3e4d308842d87e973bf1951f5f7a804b21e4dd964ecd12d6b_0 5 $feeRate"
+
+for ((i=1; i<=MINT_COUNT; i++)); do
+
     $command
 
     if [ $? -ne 0 ]; then
@@ -31,3 +36,6 @@ while true; do
 
     sleep 1
 done
+
+
+echo "所有 mint 操作完成"
